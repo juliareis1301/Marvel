e619c7a26916af9e6c642d134d0977909eecf7f0
Julia R <juliadosreissilva6@gmail.com>
2023-05-18 15:09:39 -0300

    key

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import md5 from 'md5';
 
-const publicKey = '4aff9a18901329e6f2526b0a14ac90c1';  //42defc5bb2a26a72b49da38fb15e06a4
-const privatekey = 'f7d088d6e84ad7f3947366e2893b0e06b2dfdb6a'; // 788eff2764301f0f33a60eb146f9cba2c70dce30
+const publicKey = '2ce9bdc2b73e6a6d51ebc983d2833afc';  //42defc5bb2a26a72b49da38fb15e06a4 // 4aff9a18901329e6f2526b0a14ac90c1
+const privatekey = '7cd08fbcb7f96bed53f852850d905a04143aa8ae'; // 788eff2764301f0f33a60eb146f9cba2c70dce30 //f7d088d6e84ad7f3947366e2893b0e06b2dfdb6a
 
 const ts = Number(new Date());
 
