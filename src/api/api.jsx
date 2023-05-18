2aa845d57c057fa1ac54a14cd1eff7bf4b5b22cf
Julia R <juliadosreissilva6@gmail.com>
2023-05-18 15:21:52 -0300

    atualização api

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -9,7 +9,7 @@ const ts = Number(new Date());
 const hash = md5(ts + privatekey + publicKey );
 
 const api = axios.create({
-  baseURL: `http://gateway.marvel.com/v1/public/`,
+  baseURL: `https://gateway.marvel.com/v1/public/`,
   params: {
     ts,
     apikey: publicKey,
